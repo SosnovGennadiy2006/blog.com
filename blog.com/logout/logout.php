b6cd5d9f7ce8479ad7f8966213403bb086cb9211
SosnovGennadiy2006 <67744067+SosnovGennadiy2006@users.noreply.github.com>
2021-06-30 19:58:13 +0300

    Delete blog.com directory

--- blog.com/logout/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require $_SERVER['DOCUMENT_ROOT'].'/libraries/rb.php';
-R::setup( 'mysql:host=localhost;dbname=project1', 'mysql', 'mysql' );
-
-session_start();
-unset($_SESSION['logged_user']);
-header('Location: /');
-?>
--- blog.com/scripts/scriptForMenu.js
+++
@@ -1,5 +0,0 @@
-var button = document.querySelector(".button");
-
-button.onclick = () => {
-	button.classList.toggle("active");
-}
